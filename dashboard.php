4823f556072115e11bd9ec7416f179d1f1112c30
Jacob Shim <jacobs12@umbc.edu>
2025-12-02 22:22:52 -0500

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<?php
+// STILL A WIP!!!
+// session_start();
+// require ' (sql file database name goes here) '; 
+// if (!isset($SESSION['user_id']))  header('Location: login.php'); exit(); }
+
+// $stmt = $pdo->query('SELECT * FROM users');
+// $users = $stmt->fetchAll();
+?>
+
+<html>
+    <head>
+        <title> The Admin Dashboard </title>
+        <script>
+            // Validating form data
+            function validate() {
+                let valid1 = confirm("Are you sure you want to delete this user?");
+                return valid1;
+            }
+        </script>
+    </head>
+    <body>
+        <h1> Admin Dashboard </h1>
+        <script>
+            // changing header style
+            // to be continued... 
+        </script>
+        <!-- empty space -->
+        <p> </p>
+        <!-- to be continued ... -->
+    </body>
+</html>
