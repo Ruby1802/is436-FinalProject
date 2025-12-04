22cb2c1d0ce0b1ec57b22a17a9a193d422cc0934
Jacob Shim <jacobs12@umbc.edu>
2025-12-04 12:18:28 -0500

    Update db.php

--- php_files/db.php
+++ php_files/db.php
@@ -4,7 +4,7 @@ $username = "UMBCstudent";
 $password = "bongocat123";  
 $dbname = "main_project_db";  
 
-$conn = new mysqli($servername, $username, $password, $dbname, $3308);
+$conn = new mysqli($servername, $username, $password, $dbname, 3308);
 
 if($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
